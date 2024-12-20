e9685449b881f2ba22e538999459ca0d851d8099
alfa03r <muhammadalfajar218@gmail.com>
2024-12-20 09:19:57 +0700

    Add : Adding functions.php

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "course");
+
+// if ($conn->connect_error) {
+//     die("Koneksi gagal: " . $conn->connect_error);
+// }
+// echo "Koneksi berhasil!";
+
+
+?>
